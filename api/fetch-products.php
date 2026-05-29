7d6f2ef8bc2fd2f8107cb7fe2e4f308faa0934a7
nneliifeanyi <stanvicbest@gmail.com>
2026-05-29 23:38:34 +0100

    cors iss

--- api/fetch-products.php
+++ api/fetch-products.php
@@ -2,10 +2,23 @@
 require_once '../config/db.php';
 
 // CORS
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+//header("Access-Control-Allow-Origin: *");
+$allowedOrigins = [
+    'http://localhost',
+    'http://127.0.0.1',
+    'https://budgetdroid.com.ng',
+    'https://www.budgetdroid.com.ng',
+];
+
+if (in_array($_SERVER['HTTP_ORIGIN'] ?? '', $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
+// header("Access-Control-Allow-Methods: GET, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type");
+// header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept, Origin");
+header("Access-Control-Max-Age: 3600");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
